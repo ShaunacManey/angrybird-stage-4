311955c1ec7d067ace1a9ebb6d6835d33eb2d4a9
ShaunacManey <65338229+ShaunacManey@users.noreply.github.com>
2020-08-02 16:07:49 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,23 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.visiblity = 255
+    this.image = loadImage("sprites/enemy.png");
+  }
+display (){
+  if ( this.body.speed < 3 ){
+    super.display()
+  }
+
+  else {
+    World.remove(world,this.body)
+    push();
+    tint(255,this.visiblity)
+    image(this.image,this.body.position.x,this.body.position.y,50,50)
+    pop();
+    this.visiblity=this.visiblity-5
+     }
+
+ console.log(this.body.speed)
+}
+};
